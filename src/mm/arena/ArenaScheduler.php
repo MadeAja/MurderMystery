68f45eacb651984c90c7e7fd133b4c383b76823d
DEVS <83214438+DanteDEVS@users.noreply.github.com>
2021-11-21 13:33:59 +0200

    Update ArenaScheduler.php

--- src/mm/arena/ArenaScheduler.php
+++ src/mm/arena/ArenaScheduler.php
@@ -227,14 +227,14 @@ class ArenaScheduler extends Task{
         $signText[1] = "§b{$this->plugin->map->getFolderName()} §7| §7[§b" . count($this->plugin->players) . "§7/§b16§7]";
 
         switch($this->plugin->phase){
-            case Arena::PHASE_WAITING:
-                if(count($this->plugin->players) >= 1){			
-                    $signText[2] = "§aWaiting For More Players!";
+            case Arena::PHASE_LOBBY:
+                if(count($this->plugin->players) >= 16){
+                    $signText[2] = "§cWaiting For More Players";
                     $signText[3] = "";
-                } else {		   
-                    $signText[2] = "§cNeed More Players!";
+                } else {
+                    $signText[2] = "§cWaiting";
                     $signText[3] = "";
-		}
+                }
             break;			
 		     			
             case Arena::PHASE_LOBBY:
